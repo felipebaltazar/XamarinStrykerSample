fdde5612898223cbdb9af423c72dca706e5e7db7
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2021-12-11 19:41:14 -0300

    Sample created 🎉🤘🎉🎉🎉

---
+++ StrykerSample/MyViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StrykerSample
+{
+    public class MyViewModel
+    {
+        private readonly Regex _cpfMask = new Regex(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", RegexOptions.Compiled);
+
+        public bool IsValidCPF(string cpfInput)
+        {
+            if (string.IsNullOrWhiteSpace(cpfInput))
+                return false;
+
+            return _cpfMask.Match(cpfInput).Success;
+        }
+    }
+}
